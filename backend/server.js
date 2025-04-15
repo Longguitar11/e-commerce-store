560ee1f1d5975c051a9ea8b5f8080ccc899ec2ce
Longguitar11 <bossstrong11@gmail.com>
2025-04-15 11:05:58 +0000

    fix: delete NODE_ENV in package.json

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 import { NODE_ENV, PORT } from './config/env.js';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.route.js';
@@ -8,10 +9,9 @@ import cartRoutes from './routes/cart.route.js';
 import couponRoutes from './routes/coupon.route.js';
 import paymentRoutes from './routes/order.route.js';
 import analyticsRoutes from './routes/analytics.route.js';
-import path from 'path';
 
 const app = express();
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true }));
@@ -25,10 +25,10 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if(NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
     })
 }
 
