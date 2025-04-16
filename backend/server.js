f06ffdb9ae24878880690fdb0c94e734187649f0
Longguitar11 <bossstrong11@gmail.com>
2025-04-16 03:04:33 +0000

    fix: add quotes to the route name

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use("/api/analytics", analyticsRoutes);
 if (NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*other-routes", (req, res) => {
+	app.get(`*"other-routes"`, (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
